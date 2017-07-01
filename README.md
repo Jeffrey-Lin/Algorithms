d75501ce7475dbd821d3e440b2f085add52af60c
jeffrey-lin <lin.jeffrey@hotmail.com>
2017-07-01 00:57:18 -0500

    Implement insertion sort

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Algorithms
 Algorithms from "Introduction to Algorithms" written in Python
+
+To test the run time of algorithms, run `python3 -m cProfile <file_name>.py <items to sort>`
---
+++ sorting/insertion_sort.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from sys import argv
+
+# Get user input as a command line argument
+numbers = argv[1:]
+
+for i, num in enumerate(numbers[1:], 1):
+    j = i - 1
+
+    while j >= 0 and numbers[j] > num:
+        numbers[j + 1], numbers[j] = numbers[j], num
+        j = j - 1
+
+# Print sorted list of numbers
+print(numbers)
